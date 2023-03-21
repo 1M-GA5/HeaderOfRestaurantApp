a819dd9dd99301120be2c13cfd943745d4e0c824
1M-GA5 <athrvgajbhiyexxi@gmail.com>
2023-03-22 00:29:10 +0530

    Cart open and close

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,16 @@
- import React from 'react'
- import CartIcon from '../Cart/CartIcon'
- import classes from './HeaderCartButton.module.css';
- 
- function HeaderCartButton() {
-   return <button className={classes.button}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span className={classes.badge }>Your Cart</span>
-        <span>
-             
-        </span> 
-   </button>
- }
- 
- export default HeaderCartButton
- 
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
