be5feb591137ad77750b302275a3db8d97680858
1M-GA5 <athrvgajbhiyexxi@gmail.com>
2023-03-22 14:42:13 +0530

    lete of Cart Items

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import {useRef , useState} from 'react'
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
-function MealItemForm() {
+function MealItemForm(props) {
+  const [amountIsValid,setAmountIsValid]=useState(true)
+
+  const amountInputRef = useRef();
+
+
+  const submitHandler = event =>{
+    event.preventDefault();
+
+    const enterdAmount = amountInputRef.current.value;
+    const enterdAmountNumber = +enterdAmount;
+
+
+    if(
+      enterdAmount.trim().length===0 ||
+      enterdAmountNumber < 1 ||
+      enterdAmountNumber > 5
+    ){ 
+      setAmountIsValid(false);
+      return;
+    }
+
+
+    props.onAddToCart(enterdAmountNumber);
+  }
   return (
-    <form className={classes.form}>
-        <Input label='Amount'  input={{
+    <form className={classes.form} onSubmit={submitHandler}>
+        <Input 
+            ref={amountInputRef}
+            label='Amount'  input={{
             id:'amount',
             type:'number',
             min:'1',
@@ -14,6 +40,7 @@ function MealItemForm() {
             defaultValue:'1'
         }}/>
         <button>+Add</button>
+        {!amountIsValid && <p>Enter Valid Input</p>}
     </form>
   )
 }
